23c8f676943d21a0ed9c72357105640edaa9a877
Mariella-Pasamonte <73897046+Mariella-Pasamonte@users.noreply.github.com>
2024-05-06 22:36:16 +0800

    Update editEmployeesModal.js
    Add onClick event  on XButton aside from the svg.

--- client/src/components/editEmployeesModal.js
+++ client/src/components/editEmployeesModal.js
@@ -97,6 +97,7 @@ function EditEmployeesModal(props){
                         <button
                             type="button" 
                             className='rounded'
+                            onClick={(e)=>closeModal(e)} 
                         > 
                             <svg id="XButton" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" onClick={(e)=>closeModal(e)} className="w-4 h-4">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
