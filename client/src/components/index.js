ac9005d42c622ad04156bf0d25045019080bf7bf
Mariella-Pasamonte <73897046+Mariella-Pasamonte@users.noreply.github.com>
2024-05-06 22:37:55 +0800

    Add New Feature
    TaskModal shows information about selected task.

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,13 +1,20 @@
 // Modals
+// Project features
 export {default as AddProjectModal} from './addProjectModal';
-export {default as EditEmployeesModal} from './editEmployeesModal';
 export {default as EditProjectModal} from './editProjectModal';
-export {default as WarningModal} from './warningModal';
 export {default as ProjectModal} from './projectModal';
+
+// Employee features
+export {default as EditEmployeesModal} from './editEmployeesModal';
+
+//Task features
 export {default as AddTaskModal} from './addTaskModal';
 export {default as EditTaskModal} from './editTaskModal';
+export {default as TaskModal} from './taskModal';
 
+export {default as WarningModal} from './warningModal';
 export {default as ErrorToast} from './errorToast';
+
 export {default as InputLabel} from './inputLabel';
 export {default as Popover} from './popover';
 export {default as Tooltips} from './tooltips';
--- client/src/modules/task.js
+++ client/src/modules/task.js
@@ -1,17 +1,59 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import Stopwatch from '../components/stopwatch';
+import TaskModal from '../components/editTaskModal';
 
 function Task(props){
+    let userId = parseInt(localStorage.getItem('userId'));
     const [taskFocus, setTaskFocus] = useState(null);
     const getUsername = (userId) => {
         const user = props.users&&props.users.find(user => user.id === userId);
         return user ? user.username : 'Unknown';
     };
 
-    const truncatedText =(desc)=>{
-        if(desc.length == 0) return 'N/A';
+    function getAmount(amntArr){
+        let most = 0;
+        let least = 0;
+        let temp = 0;
+        if(amntArr.length>1){
+            amntArr.map((amnt,index)=>{
+                if(most<=amnt){
+                    temp = most;
+                    most = amnt;
+                    if(index-1>=0){
+                        if(least===0){
+                            least=amntArr[index-1];
+                        }else if(least>=temp){
+                            least=amntArr[index-1];
+                        }
+                    }
+                }else{                    
+                    if(least===0){
+                        least=amnt;
+                    }else if(least>=amnt){
+                        least=amnt;
+                    }
+                }
+            })
+            if(most===least){
+                return most;
+            }else{ 
+                return least+"-"+most;
+            }
+        }
+        return amntArr[0];
+    }
+    const truncatedEmpNames =(emp)=>{
+        let empUsernames = emp.map(employee =>getUsername(employee));
+        let joinEmpUsernames = empUsernames.join(', ');
+        
+        return joinEmpUsernames.length > 15 ? joinEmpUsernames.slice(0,15) + '...' : joinEmpUsernames;
+    }
+    const truncatedDescription =(desc)=>{
+        if(desc.length === 0) return 'N/A';
         else return desc.length > 15 ? desc.slice(0, 15) + '...' : desc;
     }
+
+
     return(
         <>
             <div>
@@ -29,41 +71,29 @@ function Task(props){
                             Hour log
                         </button>
                     </div>
-                    <div className="relative flex flex-col justify-center h-full ml-2 text-white font-thin">
-                        <div className="absolute pl-2 flex pointer-events-none">
-                            <svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className='w-5 h-5'>
-                                <path fillRule="evenodd" clipRule="evenodd" d="M12 20C15.866 20 19 16.866 19 13C19 11.0824 18.229 9.34498 16.98 8.08071L18.1872 6.87353L17.1265 5.81287L15.8116 7.12778C14.9125 6.54298 13.8708 6.15908 12.7499 6.0397V4.5H15V3H9V4.5H11.2499V6.03971C10.1292 6.1591 9.08749 6.54298 8.18844 7.12773L6.87352 5.81281L5.81286 6.87347L7.02004 8.08065C5.77106 9.34493 5 11.0824 5 13C5 16.866 8.13401 20 12 20ZM12 7.5C8.96243 7.5 6.5 9.96243 6.5 13C6.5 16.0376 8.96243 18.5 12 18.5C15.0376 18.5 17.5 16.0376 17.5 13C17.5 9.96243 15.0376 7.5 12 7.5Z" fill="#D5D8E3"/>
-                                <path fillRule="evenodd" clipRule="evenodd" d="M12.5 12.5V8.99988H11V14H15V12.5H12.5Z" fill="#D5D8E3"/>
-                            </svg>       
-                        </div>
-                        <button className="font-Inter text-sm p-1 pr-3 pl-8 rounded-md bg-[#212628]/50">
-                            Customize timer
-                        </button>
-                    </div>
                 </div>
-                <div>
-                    <table className='w-fit'>
-                        <thead>
-                            <tr className='bg-[#4785C1]/60 font-Inter text-white text-sm'>
-                                <th scope="col" className=' rounded-l-md py-2 font-light w-24'>
+                <div className='overflow-hidden max-h-80 w-fit rounded-b-md'>
+                    <table className='flex flex-col w-full'>
+                        <thead className='sticky top-0'>
+                            <tr className='bg-[#4785C1] font-Inter text-white text-sm'>
+                                <th scope="col" className={`rounded-l-md font-light h-8 ${props.edit?'w-24':'w-10'}`}>
+                                </th>
+                                <th scope="col" className=' pl-2 py-2 font-light w-24 h-4'>
                                     Task Name
                                 </th>
-                                <th scope="col" className=' w-44 font-light py-2 px-5'>
+                                <th scope="col" className='  w-44 font-light py-2 px-5 h-4'>
                                     Task Description
                                 </th>
-                                <th scope="col" className=' w-24 font-light '>
-                                    Amount
+                                <th scope="col" className=' w-24 font-light h-4'>
+                                    Payment
                                 </th>
-                                <th scope="col" className=' w-32 font-light py-2 px-5'>
+                                <th scope="col" className=' w-40 font-light py-2 px-5 h-4'>
                                     Assigned
                                 </th>
-                                <th scope="col" className=' w-32 font-light py-2 px-2'>
-                                    Pending Amount
-                                </th>
-                                <th scope="col" className=' w-32 font-light py-2 px-5'>
+                                <th scope="col" className=' w-32 font-light py-2 px-5 h-4'>
                                     Status
                                 </th>
-                                <th scope="col" className=' rounded-r-md font-light py-2 w-40'>
+                                <th scope="col" className=' rounded-r-md font-light py-2 w-40 h-4'>
                                     Timer
                                 </th>
                             </tr>
@@ -72,30 +102,67 @@ function Task(props){
                             <tbody>
                                 {props.tasks.map((task)=>
                                     task.projectid === props.projectId &&
-                                    <tr key={task.id} className=' bg-[#6C93B9]/40 font-Inter h-full w-fit text-white text-sm'>
-                                        <th scope="row" className='text-center rounded-l-md py-2 font-light w-24'>
+                                    <tr onClick={(e)=>{taskFocus===task.id?setTaskFocus(null):setTaskFocus(task.id)} } className={`ml-2 font-Inter h-14 w-fit text-white text-sm ${taskFocus===task.id?'bg-[#6C93B9]/60':'bg-[#6C93B9]/40'}`}>
+                                        <td scope="row" className={`rounded-l-md font-light w-24  h-14 ${props.edit?'w-24':'w-10'}`}>
+                                            <div className=' my-2 flex flex-row justify-center h-full'>
+                                                <button
+                                                    onClick={()=>{
+                                                        props.setTask(task);
+                                                        props.setOpenTaskModal(true);
+                                                    }} 
+                                                    className='text-white'
+                                                >
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width='22' height="22" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className='ml-1 mr-1'>
+                                                        <path stroke-linecap="round" stroke-linejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
+                                                    </svg>
+                                                </button>
+                                                {props.edit&&
+                                                    <div className='flex flex-row'>
+                                                        <button
+                                                            onClick={(e)=>{
+                                                                props.setOpenEditTaskModal(true);
+                                                                props.setEmployees(task.employeelist);
+                                                                props.setEditedTask(task);
+                                                            }} 
+                                                            className='text-white'
+                                                        >
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#ffffff" viewBox="0 0 256 256" className='mr-1'>
+                                                                <path d="M225.9,74.78,181.21,30.09a14,14,0,0,0-19.8,0L38.1,153.41a13.94,13.94,0,0,0-4.1,9.9V208a14,14,0,0,0,14,14H92.69a13.94,13.94,0,0,0,9.9-4.1L225.9,94.58a14,14,0,0,0,0-19.8ZM94.1,209.41a2,2,0,0,1-1.41.59H48a2,2,0,0,1-2-2V163.31a2,2,0,0,1,.59-1.41L136,72.48,183.51,120ZM217.41,86.1,192,111.51,144.49,64,169.9,38.58a2,2,0,0,1,2.83,0l44.68,44.69a2,2,0,0,1,0,2.83Z"></path>
+                                                            </svg>
+                                                        </button>
+                                                        <button className='text-[#F02E1B]'>
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width='22' height="22" viewBox="0 0 24 24" fill="currentColor" className="">
+                                                                <path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clip-rule="evenodd" />
+                                                            </svg>
+                                                        </button> 
+                                                    </div>
+                                                }
+                                            </div>
+                                        </td>
+                                        <td scope="row" className='text-center pl-2 py-2 font-light w-24 '>
                                             {task.name}
-                                        </th>
+                                        </td>
                                         <td className=' text-center w-44 font-light py-2 px-5'>
-                                            {truncatedText(task.description)}
+                                            {truncatedDescription(task.description)}
                                         </td>
                                         <td className=' text-center w-24 font-light '>
-                                            {task.amount}
+                                            {getAmount(task.amount)}
                                         </td>
-                                        <td className=' text-center w-32 font-light py-2 px-5'>
-                                            {props.hourlog.map((hl)=>hl.taskname === task.name&&<div className='py-1'>{getUsername(hl.employeeassigned)}</div>)}
-                                        </td>
-                                        <td className=' text-center w-32 font-light py-2 px-2'>
-                                            {props.hourlog.map((hl)=>hl.taskname === task.name&&<div className='py-1'>{hl.pendingamount}</div>)}
+                                        <td className=' text-center w-40 font-light py-2 px-5'>
+                                            {truncatedEmpNames(task.employeelist)}
                                         </td>
                                         <td className=' text-center w-32 font-light py-2 px-5'>
                                             {task.status}
                                         </td>
-                                        <td className=' rounded-r-md border-1 border-black font-light py-3 w-fit'>
-                                            {props.hourlog&&props.hourlog.map((hl)=>
-                                                hl.taskname === task.name && 
-                                                <Stopwatch isOpen={task.paymenttype} id={task.id} hourlog={hl}/>
+                                        <td className='rounded-r-md border-1 border-black font-light py-3 w-40'>
+                                            {task.employeelist.map((emp)=>
+                                                emp === userId&&
+                                                <Stopwatch isOpen={task.paymenttype} task={task} hourlog={props.hourlog} userId={userId}/>
                                             )}
+                                            {/* {props.hourlog&&props.hourlog.map((hl)=>
+                                                hl.taskname === task.name&&hl.employeeassigned===userId && 
+                                                <Stopwatch isOpen={task.paymenttype} id={task.id} hourlog={hl} userId={userId}/>
+                                            )} */}
                                         </td>
                                     </tr>
                                 )}
