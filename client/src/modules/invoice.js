304e4d370d180bb631c5aba1472e93434c8ab2f4
Mariella-Pasamonte <73897046+Mariella-Pasamonte@users.noreply.github.com>
2024-05-21 03:04:57 +0800

    Update invoice.js

--- client/src/modules/invoice.js
+++ client/src/modules/invoice.js
@@ -1,75 +1,38 @@
-import React, { useState, useEffect, useContext } from "react";
-import { InvoiceSmallTemplate, AddInvoiceModal } from "../components";
-import AuthContext from "../utilities/AuthContext";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { InvoiceTemplate, InvoiceSmallTemplate } from "../components";
 import axios from "axios";
 
 
-function InvoiceModule(props) {
-  const [openAddInvoiceModal, isOpenAddInvoiceModal] = useState(false);
+function Invoice(props) {
+  const [openInvoiceTemplateModal, setOpenInvoiceTemplateModal] =
+    useState(false);
   const [isInvoiceOpen, setIsInvoiceOpen] = useState(false);
   const [invoiceFocus, setInvoiceFocus] = useState(null);
-  const {userID} = useContext(AuthContext);
+  let userId = parseInt(localStorage.getItem("userId"));
 
-  const validProjects = props.projects && props.projects.length > 0 ? props.projects : [];
-  const validInvoices = props.invoices && props.invoices.length > 0 ? props.invoices : [];
-  const validTasks = props.tasks && props.tasks.length > 0 ? props.tasks : [];
-  const validUsers = props.users && props.users.length > 0 ? props.users : [];
+  const validProject = Array.isArray(props.projects) && props.projects.length > 0 ? props.projects : [];
+  const validInvoices = Array.isArray(props.invoices) && props.invoices.length > 0 ? props.invoices : [];
+  const validTasks = Array.isArray(props.tasks) && props.tasks.length > 0 ? props.tasks : [];
 
-  const projectInvoices = validProjects.map((project)=>validInvoices.filter(invoice => invoice.invoice_project === project.id));
-  const tasks = validTasks.filter(tasks => tasks.status === "Finished");
-  const user = validUsers.filter((user)=>user.id===userID);
+  const projectInvoices = validInvoices.filter(invoice => invoice.invoice_project === props.projects.id);
+  const projects = props.projects;
+  const tasks = validTasks.filter(tasks => tasks.status === "Finished" && tasks.projectid === props.projects.id);
+  const user = props.user;
   const hourlog = props.hourlog;
-  
-  function addNewInvoice(projectid){
-    const taskids = validTasks.filter((task)=>task.projectid===projectid&&task.status==="Finished"&&task.id)
-    console.log(taskids);
-    // const hourlogs = props.hourlog.map((hl)=>
-    //     hl.taskid===taskId&&hl.employeeassigned===userID&&hl.id
-    // )
-    // const filteredHourlogs = props.hourlog.filter(
-    //     (item) =>
-    //         item.employeeassigned === userID && item.taskid === taskId
-    // );
-    // let total = filteredHourlogs.reduce((acc, hl) => acc+parseFloat(hl.pendingamount),0).toFixed(2);
-    // const newInvoice = {
-    //   invoice_number: props.invoices?props.invoices.length+1:1,
-    //   invoice_to_clientName: props.project.clientname,
-    //   invoice_to_clientEmAdd: props.project.clientemadd,
-    //   invoice_from_userId: userID,
-    //   notes: "Thank you for your service",
-    //   invoice_project:props.project.id,
-    //   invoice_tasks: taskids,
-    //   invoice_hourlogs: hourlogs,
-    //   invoice_total: parseFloat(total)
-    // }
-    // axios
-    // // .post("https://dowee2-server2.vercel.app/generateInvoice")
-    // .post("http://localhost:3000/generateInvoice", newInvoice)
-    // .then((res))
-    // .catch((error) => {
-    //     console.log(error);
-    // });
-        // if (res.data.status===true){
-        //     const invoice = res.data.invoice;
-        //     axios
-        //     // .post(`{https://dowee2-server2.vercel.app/updateInvoice/${userID}}`, invoice)
-        //     .post(`{http://localhost:3000/updateInvoice/${userID}}`, invoice)
-        //     .then((res))
-        //     .catch((error) => {
-        //         console.log(error);
-        //     }); ; 
-  }
+
+  console.log("invoices",projectInvoices)
+  console.log("tasks",tasks)
 
   return (
     <>
       <div>
-        <AddInvoiceModal isOpen={openAddInvoiceModal} closeModal={isOpenAddInvoiceModal} projects={validProjects} tasks={tasks} hourlog={props.hourlog}/>
         <div className="flex flex-row w-96 h-full">
           <div className="text-xl mb-2 mr-4 w-fit font-medium text-white">
             Project Invoice
           </div>
           <div className="relative flex flex-col justify-center h-full ml-2 text-white font-thin">
-            <button onClick={()=>isOpenAddInvoiceModal(true)} className="font-Inter text-sm py-1 px-3 rounded-md bg-[#212628]/50">
+            <button className="font-Inter text-sm py-1 px-3 rounded-md bg-[#212628]/50">
               Generate Invoice
             </button>
           </div>
@@ -98,12 +61,14 @@ function InvoiceModule(props) {
           </nav>
         </div>
         <div className="flex h-[30rem] xl:h-full">
-          <div className="w-full h-full border-[#AEAEE3] border-[1px]  rounded-lg  flex flex-row gap-5 xl:gap-x-5 xl:gap-y-3 2xl:gap-5 px-3 justify-center flex-wrap">
-            {validInvoices.map(
-              (invoice) =>
-                <InvoiceSmallTemplate invoice={invoice} projects={validProjects} tasks={tasks} user={user} hourlog={hourlog}/>
-            )}
-          </div>
+          {projectInvoices && projects && tasks && user && hourlog &&(
+            <div className="w-full h-full border-[#AEAEE3] border-[1px]  rounded-lg  flex flex-row gap-5 xl:gap-x-5 xl:gap-y-3 2xl:gap-5 px-3 justify-center flex-wrap">
+              {projectInvoices.map(
+                (invoice) =>
+                  <InvoiceSmallTemplate invoice={invoice} projects={projects} tasks={tasks} user={user} hourlog={hourlog}/>
+              )}
+            </div>
+          )}
           {/* <InvoiceTemplate /> */}
           {/* <div className="w-full h-full border-[#AEAEE3] border-[1px]  rounded-lg  flex flex-row gap-5 xl:gap-x-5 xl:gap-y-3 2xl:gap-5 px-3 justify-center flex-wrap">
               <InvoiceSmallTemplate />
@@ -119,4 +84,4 @@ function InvoiceModule(props) {
   );
 }
 
-export default InvoiceModule;
+export default Invoice;
